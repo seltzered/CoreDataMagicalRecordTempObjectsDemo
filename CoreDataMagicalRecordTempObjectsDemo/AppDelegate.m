befa602ec42c0929b6de1721a0494308a293fef1
Vivek Gani <vivek@gani.org>
2014-06-23 21:47:10 -0700

    Initial Commit

---
+++ CoreDataMagicalRecordTempObjectsDemo/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  CoreDataMagicalRecordTempObjectsDemo
+//
+//  Created by Vivek Gani on 6/23/14.
+//  Copyright (c) 2014 Vivek Gani. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ CoreDataMagicalRecordTempObjectsDemo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CoreDataMagicalRecordTempObjectsDemo
+//
+//  Created by Vivek Gani on 6/23/14.
+//  Copyright (c) 2014 Vivek Gani. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
